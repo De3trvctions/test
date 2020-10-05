31263f190c18e865ab6cd4fee36b31d14c20a47c
Kel Den <keldenong@gmail.com>
2020-10-05 10:06:11 +0800

    asd

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 app.get('/hi', function (req, res) {
-  res.send('Hello !!!');
+  res.send('Hello Boy!!!');
 });
 
 app.listen(3000, function () {
